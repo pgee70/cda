6bbc6676df95c87044bbec9e35a51fbdcd9e07c4
pgee70 <pgee70@users.noreply.github.com>
2018-11-20 21:14:05 +1100

    Update Autoload.php

--- lib/Autoload.php
+++ lib/Autoload.php
@@ -22,3 +22,41 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+namespace i3Soft\CDA;
+
+/**
+ * Autoloader
+ */
+class Autoloader
+{
+  /** @const string */
+  const NAMESPACE_PREFIX = 'i3Soft\\CDA\\';
+
+  /**
+   * Register
+   *
+   * @return void
+   */
+  public static function register()
+  {
+    spl_autoload_register(array(new self, 'autoload'));
+  }
+
+  /**
+   * Autoload
+   *
+   * @param string $class
+   */
+  public static function autoload($class)
+  {
+    $prefixLength = strlen(self::NAMESPACE_PREFIX);
+    if (0 === strncmp(self::NAMESPACE_PREFIX, $class, $prefixLength)) {
+      $file = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, $prefixLength));
+      $file = realpath(__DIR__ . (empty($file) ? '' : DIRECTORY_SEPARATOR) . $file . '.php');
+      if (file_exists($file)) {
+        /** @noinspection PhpIncludeInspection Dynamic includes */
+        require_once $file;
+      }
+    }
+  }
+}
